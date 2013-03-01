e9b353471b5ae107b90eb3db3e2d2d0c24d889c5
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-03-01 16:40:49 +0100

    Added googleplusapi. Fixed plugins path. First successful gplus call.

--- js/app.js
+++ js/app.js
@@ -8,10 +8,15 @@ require.config({
     //the paths config could be for a directory.	
     paths: {
     	app: '../app',
-    	plugin: "../plugins",
+    	plugin: '../plugins',
+    	
     	jquery: "http://code.jquery.com/jquery-1.9.1.min.js",
         knockout: "knockout-2.2.1",
         bootstrap: "boostrap.min",
+        
+        goog: '../plugins/goog',
+        async: '../plugins/async',
+        propertyParser: '../plugins/propertyParser',
     },
     waitSeconds: 10,
     shim: {
@@ -19,13 +24,23 @@ require.config({
     }
   });
     
-require(['knockout', 'plugin/domReady!'], function(ko) {
+require(['knockout', 'googleplusapi.compressed', 'plugin/domReady!'], function(ko, goog) {
+	
+    var apiKey = 'AIzaSyCpdmuyNHOGzOvLCsN2OmtZ7w_z-3wxnn8';
+    var userId = '101695111306977669026';	
+	var GoogleAPI = new GooglePlusAPI(apiKey);
 	
 	var myViewModel = {
 		    personName: 'Bob',
 		    personAge: 123
 		};
+	
+	GoogleAPI.getPerson(userId, {}, function(error, result) {
+		console.log(result);
+		myViewModel.personName = result.displayName;
+		ko.applyBindings(myViewModel);
+	});
 
-	ko.applyBindings(myViewModel);	
+		
 	
 });
