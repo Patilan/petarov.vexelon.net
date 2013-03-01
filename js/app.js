5f5fea6268270c31e5e0d9d540b808d796a6a64a
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-03-01 18:33:07 +0100

    Fetching latest profile gplus posts and using templates to show content.

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ require.config({
     	app: '../app',
     	plugin: '../plugins',
     	
-    	jquery: "http://code.jquery.com/jquery-1.9.1.min.js",
-        knockout: "knockout-2.2.1",
-        bootstrap: "boostrap.min",
-        
+    	jquery: "http://code.jquery.com/jquery-1.9.1.min",
+        knockout: 'knockout-2.2.1',
+        bootstrap: 'bootstrap.min',
+        // Require JS plugins
         goog: '../plugins/goog',
         async: '../plugins/async',
         propertyParser: '../plugins/propertyParser',
@@ -24,7 +24,7 @@ require.config({
     }
   });
     
-require(['knockout', 'googleplusapi.compressed', 'plugin/domReady!'], function(ko, goog) {
+require(['knockout', 'bootstrap', 'googleplusapi.compressed', 'plugin/domReady!'], function(ko, goog) {
 	
     var apiKey = 'AIzaSyCpdmuyNHOGzOvLCsN2OmtZ7w_z-3wxnn8';
     var userId = '101695111306977669026';	
@@ -35,12 +35,28 @@ require(['knockout', 'googleplusapi.compressed', 'plugin/domReady!'], function(k
 		    personAge: 123
 		};
 	
-	GoogleAPI.getPerson(userId, {}, function(error, result) {
+//	GoogleAPI.getPerson(userId, {}, function(error, result) {
+//		console.log(result);
+//		myViewModel.personName = result.displayName;
+//		ko.applyBindings(myViewModel);
+//	});
+	
+	GoogleAPI.listActivities(userId, {'maxResults': 3}, function(error, result) {
 		console.log(result);
-		myViewModel.personName = result.displayName;
-		ko.applyBindings(myViewModel);
-	});
-
+		
+		var posts1 = [];
+		
+		for (var i = 0; i < result.items.length; i++) {
+			posts1.push( {content: result.items[i].object.content, link: result.items[i].url } );
+		}
+		
+		ko.applyBindings({
+				posts: ko.observableArray(posts1),
+				clickr: function(url) {
+					window.location.href = url.link;
+				}
+		});
+	});	
 		
 	
 });
