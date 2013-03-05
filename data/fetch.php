47cca5a20fb609b116f712e91acbd2d04247d5fe
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-03-05 23:48:07 +0100

    Fixed access to json files. Updated htaccess rules.

--- data/fetch.php
+++ data/fetch.php
@@ -1,16 +1,23 @@
 <?php
 $who = $_GET['w'];
-
+if (empty($_GET['callback'])) {
+	header("HTTP/1.0 500 Internal Server Error");
+	die("Missing jsonp parameter!");
+}
+$callback = $_GET['callback'];
+$file = '';
 switch($who) {
 	case 'games':
-		echo $_GET['callback'] . '(' . file_get_contents('games.json') . ')';
+		$file = 'games.json';
 		break;
 	case 'oss':
-		echo $_GET['callback'] . '(' . file_get_contents('oss.json') . ')';
+		$file = 'oss.json';
 		break;		
 	default:
-		//header("HTTP/1.0 404 Not Found");
+		header("HTTP/1.0 404 Not Found");
 		die("not found $who");
 		break;
 }
+// spit it
+echo $_GET['callback'] . '(' . file_get_contents($file) . ')';
 ?>
