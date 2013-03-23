87326add28f8647a5edf2bc06053579159d625da
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-03-23 15:25:01 +0100

    Added index.php due to Heroku's requirements

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file exists only due to Heroku's requirement 
+ * to have an index.php file when pushing a PHP framework 
+ * type of project to the cedar stack 
+ */
+header('Location: index.html');
+?>
+
